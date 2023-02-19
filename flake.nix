3375581355b59267588a38c5f50959486bd52147
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-02-18 19:38:17 -0500

    build(flake): switch to flake-utils

--- flake.nix
+++ flake.nix
@@ -2,55 +2,54 @@
   description = "Blueprint";
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs@{ self, nixpkgs, utils, fenix, ... }: utils.lib.mkFlake {
-    inherit self inputs;
-    sharedOverlays = [ fenix.overlays.default ];
-    outputsBuilder = channels:
-      let pkgs = channels.nixpkgs; in {
-        devShells = {
-          default = pkgs.mkShell {
-            name = "blueprint";
-            packages = with pkgs; [
-              cargo
-              rustc
-              clippy
-              rustfmt
-              rust-analyzer
-              cargo-unused-features
-              cargo-bloat
-              cargo-flamegraph
-              hyperfine
-              valgrind
-              nixpkgs-fmt
-              nil
-            ];
-          };
-          nightly = pkgs.mkShell {
-            name = "blueprint-nightly";
-            packages = with pkgs; [
-              (fenix.packages.x86_64-linux.complete.withComponents [
-                "cargo"
-                "rustc"
-                "clippy"
-                "rustfmt"
-                "rust-src"
-              ])
-              rust-analyzer-nightly
-              cargo-unused-features
-              cargo-bloat
-              cargo-flamegraph
-              hyperfine
-              nixpkgs-fmt
-              nil
-            ];
-          };
-        };
+  outputs = { self, nixpkgs, utils, fenix, ... }: utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells = {
+      default = pkgs.mkShell {
+        name = "blueprint";
+        packages = with pkgs; [
+          cargo
+          rustc
+          clippy
+          rustfmt
+          just
+          rust-analyzer
+          cargo-unused-features
+          cargo-bloat
+          cargo-flamegraph
+          hyperfine
+          valgrind
+          nixpkgs-fmt
+          nil
+        ];
       };
-  };
+      nightly = pkgs.mkShell {
+        name = "blueprint-nightly";
+        packages = with pkgs; [
+          (fenix.packages.x86_64-linux.complete.withComponents [
+            "cargo"
+            "rustc"
+            "rust-src"
+          ])
+          just
+        ];
+      };
+    };
+
+    packages.default = pkgs.rustPlatform.buildRustPackage {
+      pname = "blueprint-scheme";
+      version = "0.1.0";
+      cargoLock.lockFile = ./Cargo.lock;
+      src = ./.;
+    };
+  });
 }
